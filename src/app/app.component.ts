5e3e66551dfee35dc72f252d82ff7986481c1aa9
Brainedsnarky <stead.official@gmail.com>
2017-04-03 11:10:29 -0700

    ini

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  //userData = [];
+  userData : Promise<any[]>;
+  
+  constructor(private _data : DataService) {}
+
+  ngOnInit(){
+
+    /*this._data.getData()
+    .subscribe(res => this.userData = res);*/
+
+    this.userData = this._data.getData();
+
+  }
+
 }
